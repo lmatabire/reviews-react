1a9eb4d02f76d8d6b457f9b06c09317929bf401d
Lee Matabire <Matabire23@gmail.com>
2018-02-15 05:47:22 -0600

    added react

--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,40 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+//user mongoose model
+passport.serializeUser((user, done) =>  {
+    done(null, user.id);
+});
+// 
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
 
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-        }, 
+        },
         (accessToken, refreshToken, profile, done) => {
-            console.log('accessToken: ', accessToken);
-            console.log('refreshToken: ', refreshToken);
-            console.log('profile ', profile);
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if(existingUser) {
+                        // we already have a record with the given profile ID
+                        done(null, existingUser);
+                    } else {
+                        // we dont have a user record with this ID, make a new record ID
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                })
         }
     )   
 );
